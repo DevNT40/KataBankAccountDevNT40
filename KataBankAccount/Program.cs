dc97475bd63e07620cc01d74ed6cf7f20433868d
Dev NT40 <devnt40@gmail.com>
2022-03-04 17:17:12 +0100

    Repond au besoin de l'US 1:Afin de mettre de l'argent de côté En tant que client d'une banque Je voudrais faire un dépôt sur mon compte

--- KataBankAccount/Program.cs
+++ KataBankAccount/Program.cs
@@ -6,6 +6,16 @@ namespace KataBankAccount
     {
         static void Main(string[] args)
         {
+            var accountService = new AccountService(
+                new TransactionRepository(new DateTimeProvider()),
+                new StatementPrinter(new Console()));
+
+            //Test de l'US 1
+            accountService.Deposit(1000);
+            accountService.Deposit(500);
+            accountService.Deposit(200);
+
+            accountService.PrintStatement();
             System.Console.ReadKey();
         }
     }
---
+++ KataBankAccount/TransactionRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataBankAccount
+{
+    public interface ITransactionRepository
+    {
+        void AddDeposit(int amount);
+        IReadOnlyList<Transaction> GetAll();
+    }
+
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly List<Transaction> _transactions = new List<Transaction>();
+
+        public TransactionRepository(IDateTimeProvider dateTimeProvider) => _dateTimeProvider = dateTimeProvider;
+
+        public void AddDeposit(int amount) => _transactions.Add(new Transaction(_dateTimeProvider.FormattedCurrentDate, amount));
+
+        public IReadOnlyList<Transaction> GetAll()
+        {
+            return _transactions;
+        }
+    }
+}
