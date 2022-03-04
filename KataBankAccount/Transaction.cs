8046a22a3aab1e3d56292593847860ce08839179
Dev NT40 <devnt40@gmail.com>
2022-03-04 17:01:27 +0100

    feature: mise en place de la classe Transaction

---
+++ KataBankAccount/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataBankAccount
+{
+    public class Transaction
+    {
+        public int Amount { get; }
+        public string Date { get; }
+
+        public Transaction(string date, int amount)
+        {
+            Date = date;
+            Amount = amount;
+        }
+    }
+}
